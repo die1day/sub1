b4377420b33efe106ee11fa370eb2840c976eaaf
Jeff <jeffyull@hotmail.com>
2022-06-05 09:19:22 +1000

    i7 change main's sub1 after pull

--- code_sub1.py
+++ code_sub1.py
@@ -9,6 +9,8 @@ import numpy as np
 def cal(x, y):
     if x != 0 and y != 0:
         return np.log(x) * np.exp(y) * 999 - np.log(y)
+    elif x > 100:
+        return 0
     else:
         return -333
     
